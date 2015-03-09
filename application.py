175ab5231dc8a700156bf6774cc3350d605d6014
Xueqi Chen <xc1611@ic.ac.uk>
2015-03-09 10:15:08 +0000

    added spain currency

--- application.py
+++ application.py
@@ -50,6 +50,8 @@ def api(args):
         return "Paris"
     elif "James Bond" in q:
         return "Sean Connery"
+    elif "currency" in q and "Spain":
+        return "peseta"
     return -1
 
 def largest(numbers):
