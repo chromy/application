fd9b9f6d92cd04804fe4850570d73cb67e225098
Hector Dearman <hector.dearman@gmail.com>
2015-03-09 09:15:13 +0000

    add api

--- application.py
+++ application.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/<int:n>')
-def index(n):
-    response = fizzbuzz(n)
-    return render_template('index.html', response=response)
-
-def fizzbuzz(n):
-    if n % 3 == 0:
-        return 'Fizz'
-    return ''
+@app.route('/api')
+def index():
+    q = request.args.get('q')
+    print 'q', q
+    print 'args', request.args
+    return render_template('index.html', response=1)
 
 if __name__ == '__main__':
     app.run(debug=True)
