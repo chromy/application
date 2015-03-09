be82ea466a1f459f7b987c9a4d3c459a9519f1aa
Hector Dearman <hector.dearman@gmail.com>
2015-03-09 09:29:37 +0000

    largest

--- application.py
+++ application.py
@@ -6,7 +6,27 @@ def index():
     q = request.args.get('q')
     print 'q', q
     print 'args', request.args
-    return render_template('index.html', response=1)
+    return str(api(request.args))
+
+def api(args):
+    hash = args['q'].split(':')[0]
+    text = args['q'].split(':')[1]
+    numbers = args['q'].split(':')[2]
+    numbers = map(int, numbers.split(', '))
+    if "largest" in text:
+        return largest(numbers)
+    elif "plus" in text:
+        return plus(numbers)
+    return ""
+
+def largest(numbers):
+    return max(numbers)
+
+def plus(numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    return total
 
 if __name__ == '__main__':
     app.run(debug=True)
